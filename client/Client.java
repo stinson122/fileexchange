db495038916893eb7fb29807660b95dfe5e4d840
seulbound <fonnymeem@gmail.com>
2024-07-31 18:03:42 +0800

    fixed /dir when server closed error

--- client/Client.java
+++ client/Client.java
@@ -65,7 +65,7 @@ public class Client {
       try {
         DataOutputStream writer = new DataOutputStream(endpoint.getOutputStream());
         DataInputStream reader = new DataInputStream(endpoint.getInputStream());
-                
+
         while (flag) {
           System.out.print("> ");
 
@@ -75,20 +75,20 @@ public class Client {
           command = tempString[0];
 
           if (command.equals("/register")) {
-            if(clientName == null){
+            if (clientName == null) {
               try {
                 String username = tempString[1];
                 msg = command + " " + username;
                 writer.writeUTF(msg);
                 String response = reader.readUTF();
                 System.out.println(response);
-                if(response.startsWith("Welcome")) {
+                if (response.startsWith("Welcome")) {
                   clientName = username;
                   filesDir = workingDir + "/" + clientName + "/files";
                   new File(filesDir).mkdirs();
                 }
               } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("Error: Command parameters do not match or is not allowed."); 
+                System.out.println("Error: Command parameters do not match or is not allowed.");
               }
             } else {
               System.out.println("Error: Command not found.");
@@ -99,24 +99,25 @@ public class Client {
             msg = command + " " + clientName;
             writer.writeUTF(msg);
             break;
-          } else if (clientName == null){
+          } else if (clientName == null) {
             System.out.println("Error: Command not found.");
           } else {
-                      
-            if ((command.equals("/store"))) { 
+
+            if ((command.equals("/store"))) {
               try {
-                File tempFile = new File(filesDir,tempString[1]);
+                File tempFile = new File(filesDir, tempString[1]);
                 if (tempFile.isFile()) {
-                  String timeStamp = ZonedDateTime.now(ZoneId.of( "Asia/Shanghai")).format(DateTimeFormatter.ofPattern( "uuuu-MM-dd.HH.mm.ss"));
+                  String timeStamp = ZonedDateTime.now(ZoneId.of("Asia/Shanghai"))
+                      .format(DateTimeFormatter.ofPattern("uuuu-MM-dd.HH.mm.ss"));
                   msg = command + " " + (tempString[1]) + " " + timeStamp + " " + clientName;
                   writer.writeUTF(msg);
 
                   DataInputStream dis = new DataInputStream(new FileInputStream(tempFile));
-                                            
+
                   byte[] filebyte = new byte[byteCapacity];
                   int file = dis.read(filebyte, 0, filebyte.length);
-                  writer.write(filebyte, 0, file);                                
-                    
+                  writer.write(filebyte, 0, file);
+
                   System.out.println(clientName + "<" + timeStamp + ">" + ": Uploaded " + tempString[1]);
                   dis.close();
                 } else {
@@ -126,26 +127,26 @@ public class Client {
                 System.out.println("Error: Command parameters do not match or is not allowed.");
               }
 
-            } else if (command.equals("/get")) { 
-              try {                      
+            } else if (command.equals("/get")) {
+              try {
                 msg = command + " " + tempString[1];
                 writer.writeUTF(msg);
 
                 String reply = reader.readUTF();
                 if (reply.equals("File received")) {
-                  File tempFile = new File(filesDir,tempString[1]);
+                  File tempFile = new File(filesDir, tempString[1]);
                   DataOutputStream dos = new DataOutputStream(new FileOutputStream(tempFile));
 
                   byte[] filebyte = new byte[byteCapacity];
                   int file = reader.read(filebyte, 0, filebyte.length);
-                  dos.write(filebyte, 0, file);     
-                  writer.writeUTF("File '" + tempString[1] + "' sent successfully to " + clientName); 
+                  dos.write(filebyte, 0, file);
+                  writer.writeUTF("File '" + tempString[1] + "' sent successfully to " + clientName);
                   System.out.println("File received form Server: " + tempString[1]);
                   dos.close();
                 } else {
                   System.out.println("Error: File not found in the server.");
-                }             
-              } catch (ArrayIndexOutOfBoundsException e)  {
+                }
+              } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Please enter file path of file");
               }
             } else if (command.equals("/leave")) {
@@ -157,28 +158,30 @@ public class Client {
               String directory = reader.readUTF();
               System.out.println("Server Directory");
               System.out.println(directory);
-                                
+
             } else if (command.equals("/?")) {
               msg = "/?";
               writer.writeUTF(msg);
               System.out.println(reader.readUTF());
-        
+
             } else {
               System.out.println("Error: Command not found.");
             }
           }
         }
-    
-          sc.close();
-          endpoint.close();
-        } catch (SocketException e) {
-          System.out.println("Server has stopped");
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-            
-        System.out.println("Connection closed. Thank you!");
+
+        sc.close();
+        endpoint.close();
+      } catch (SocketException e) {
+        System.out.println("Server has stopped");
+      } catch (EOFException e) {
+        System.out.println("Server has stopped");
+      } catch (Exception e) {
+        e.printStackTrace();
       }
+
+      System.out.println("Connection closed. Thank you!");
     }
-    
   }
+
+}
